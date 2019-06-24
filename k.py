2c08d4d2cfe627d400bca7ed1f4e3bae57767dcc
Grim Hacker 555 <51239639+Grim-Hacker-555@users.noreply.github.com>
2019-06-24 21:08:20 +0700

    Update and rename README.md to k.py

---
+++ k.py
@@ -0,0 +1,19 @@
+# Calculate
+yy = input ("                 ****  : ")
+dd = input ("                 ****  : ")
+print ("           1 = a*\n")
+print ("           2 = /\n")
+print ("           3 = *\n")
+print ("           4 = +\n")
+print ("           5 = -\n")
+gg = input ("     00000 :")
+if gg == 1 :
+    print (yy**dd)
+if gg == 2 :
+    print (yy/dd)
+if gg == 3 :
+    print (yy*dd)
+if gg == 4 :
+    print (yy+dd)
+if gg == 5 :
+    print (yy-dd)
